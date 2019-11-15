eb95058de65489771499ee30f27aeeecace0650e
guilherme otsu <guiotsu@gmail.com>
2019-11-15 00:37:19 -0300

    refatorando código

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: ListaTransferencias(),
           appBar: AppBar(
             title: Text('Tranferências'),
           ),
@@ -12,3 +13,42 @@ void main() => runApp(
         ),
       ),
     );
+
+// classe para refatorar o código
+class ListaTransferencias extends StatelessWidget {
+  // sobrescrever o método obrigatório
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(200.0, 1001)),
+        ItemTransferencia(Transferencia(300.0, 100)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  // final -> valor constante
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
