99798775bfb0a25ce977d710605fc6b4ba700ade
guilherme otsu <guiotsu@gmail.com>
2019-11-15 18:46:17 -0300

    refatorando codigo

--- lib/main.dart
+++ lib/main.dart
@@ -26,52 +26,58 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
-            child: TextField(
-              controller:
-                  _controllerNumeroConta, // faz com que pegue os valores do campos
-              style: TextStyle(fontSize: 24.0),
-              decoration: InputDecoration(
-                labelText: 'Numero da conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 54.0),
-            child: TextField(
-              controller: _controllerValor,
-              style: TextStyle(fontSize: 24.0),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0.00',
-              ),
-              keyboardType: TextInputType.number,
-            ),
+          Editor(
+              controlador: _controllerNumeroConta,
+              rotulo: 'Numero da conta',
+              dica: '0000'),
+          Editor(
+            controlador: _controllerValor,
+            rotulo: 'Valor',
+            dica: '000.0',
+            icone: Icons.monetization_on,
           ),
           RaisedButton(
-            onPressed: () {
-              // fazendo a conversão de valores
-              final int numeroConta = int.tryParse(_controllerNumeroConta.text);
-              final double valor = double.tryParse(_controllerValor.text);
-
-              if (numeroConta != null && valor != null) {
-                final transferenciaCriada = Transferencia(valor, numeroConta);
-
-                debugPrint('$transferenciaCriada');
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('$transferenciaCriada'),
-                  ),
-                );
-              }
-            },
+            onPressed: () => _criaTransferencia(),
             child: Text('Confirmar'),
           ),
         ],
+      ),
+    );
+  }
+
+  void _criaTransferencia() {
+    // fazendo a conversão de valores
+    final int numeroConta = int.tryParse(_controllerNumeroConta.text);
+    final double valor = double.tryParse(_controllerValor.text);
+
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+    }
+  }
+}
+
+// classe para criar os campos do formulário
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador, // faz com que pegue os valores do campos
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
